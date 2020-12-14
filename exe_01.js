ee977e35890fdebbfb9de0cf60c258c2b39b06cd
Salmon <skholmatov124@gmail.com>
2020-12-14 13:50:20 +0500

    first commit

---
+++ exe_01.js
@@ -0,0 +1,35 @@
+//1. Create a function that takes two numbers as arguments and return their sum.
+function sum(x, y){
+  console.log(x + y);
+}
+sum(2, 5)
+
+
+
+
+
+//2. Write a function that takes an integer minutes and converts it to seconds.
+function convert(number){
+  console.log(number *60);
+}
+convert(5)
+
+//3. Create a function that takes the age and return the age in days.
+function ageInDays(age){
+  console.log(age * 365);
+}
+ageInDays(20)
+
+
+//4. Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+function increment(x){
+  console.log(x + 1)
+}
+increment(1)
+
+//5. Write a function that takes the base and height of a triangle and return its area.
+function triangleArea(base, height){
+  var area = (base * height)/2;
+  console.log(area);
+}
+  triangleArea(6, 5)
